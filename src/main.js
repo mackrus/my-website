3c2b5d2195d1aa299376caea1a670b2a1ed98bdf
Markus Bajlo <markusbajlo@gmail.com>
2023-12-28 16:11:04 +1100

    Added CV file, .ico and updated content in index

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ scene.add(sunGroup);
 // Renderer configuration
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.set(0, 0, 10);
 
 // Lighting
 const pointLight = new THREE.PointLight(0xffffff);
@@ -28,24 +28,29 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 // Sun
 const sun = createCelestialBody('photos/sun.jpg', 5);
+
 sunGroup.add(sun);
 
-// Planets
+// Load textures for each planet photos are from https://www.solarsystemscope.com/textures/
+// Planets with more realistic proportions and distances (not to scale)
 const planetsInfo = [
-    { name: 'Mercury', texture: 'photos/mercury.jpg', size: 0.04, orbitRadius: 1.45, orbitSpeed: 0.44 },
-    { name: 'Venus', texture: 'photos/venus.jpg', size: 0.1, orbitRadius: 2.7, orbitSpeed: 0.35 },
-    { name: 'Earth', texture: 'photos/earth.jpg', size: 0.1, orbitRadius: 3.9, orbitSpeed: 0.3 },
-    { name: 'Mars', texture: 'photos/mars.jpg', size: 0.053, orbitRadius: 5.8, orbitSpeed: 0.24 },
-    { name: 'Jupiter', texture: 'photos/jupiter.jpg', size: 1.1, orbitRadius: 19.5, orbitSpeed: 0.13 },
-    { name: 'Saturn', texture: 'photos/saturn.jpg', size: 0.9, orbitRadius: 36.2, orbitSpeed: 0.1 },
-    { name: 'Uranus', texture: 'photos/uranus.jpg', size: 0.4, orbitRadius: 72.6, orbitSpeed: 0.07 },
-    { name: 'Neptune', texture: 'photos/neptune.jpg', size: 0.38, orbitRadius: 115.2, orbitSpeed: 0.06 },
-    { name: 'Pluto', texture: 'photos/pluto.jpg', size: 0.15, orbitRadius: 145.2, orbitSpeed: 0.04 },
+    { name: 'Mercury', texture: 'photos/mercury.jpg', size: 0.383, orbitRadius: 0.69, orbitSpeed: 0.74 },
+    { name: 'Venus', texture: 'photos/venus.jpg', size: 0.949, orbitRadius: 0.92, orbitSpeed: 0.85 },
+    { name: 'Earth', texture: 'photos/earth.jpg', size: 1, orbitRadius: 1.1, orbitSpeed: 0.98 },
+    { name: 'Mars', texture: 'photos/mars.jpg', size: 0.532, orbitRadius: 1.52, orbitSpeed: 1.1 },
+    { name: 'Jupiter', texture: 'photos/jupiter.jpg', size: 11.21, orbitRadius: 3.2, orbitSpeed: 1.31 },
+    { name: 'Saturn', texture: 'photos/saturn.jpg', size: 9.45, orbitRadius: 5.58, orbitSpeed: 1.97 },
+    { name: 'Uranus', texture: 'photos/uranus.jpg', size: 4.01, orbitRadius: 10.22, orbitSpeed: 2.68 },
+    { name: 'Neptune', texture: 'photos/neptune.jpg', size: 3.88, orbitRadius: 17.05, orbitSpeed: 3.54 },
+    { name: 'Pluto', texture: 'photos/pluto.jpg', size: 0.186, orbitRadius: 22.48, orbitSpeed: 4.47 },
 ];
 
-// Add rotation speeds to each planet's data
+// Add rotation speeds to each planet's data and tune/scale the other parameters
 planetsInfo.forEach(planetInfo => {
-    planetInfo.rotationSpeeds = { x: 0.001, y: 0.002, z: 0.001 }; // Example values, adjust as needed
+    planetInfo.rotationSpeeds = { x: 0.002, y: 0.002, z: 0.00001 }; // Example values, adjust as needed
+    planetInfo.orbitRadius *= 15;
+    planetInfo.size /= 8;
+    planetInfo.orbitSpeed /= 5; 
 });
 
 const planets = planetsInfo.map(info => {
@@ -53,6 +58,13 @@ const planets = planetsInfo.map(info => {
     sunGroup.add(planet.object);
     return planet;
 });
+
+const saturn = planets.find(planet => planet.name === 'Saturn');
+if (saturn) {
+    const saturnRing = createSaturnRing('photos/saturn_ring.png', saturn.size);
+    saturn.object.add(saturnRing);
+};
+
 
 
 
@@ -63,8 +75,11 @@ createStars(500);
 function animate() {
     requestAnimationFrame(animate);
 
+    rotateSun();
+
     // Update celestial bodies
     planets.forEach(planet => {
+        
         updatePlanet(planet);
         rotatePlanet(planet.mesh, planet.rotationSpeeds);
     });
@@ -84,27 +99,31 @@ function createCelestialBody(texturePath, size) {
     const texture = textureLoader.load(texturePath);
     const material = new THREE.MeshBasicMaterial({ map: texture });
     return new THREE.Mesh(geometry, material);
+    
 }
 
-// function createPlanet({ name, texture, size, orbitRadius, orbitSpeed }) {
-//     const planetGeometry = new THREE.SphereGeometry(size, 32, 32);
-//     const planetTexture = textureLoader.load(texture);
-//     const planetMaterial = new THREE.MeshBasicMaterial({ map: planetTexture });
-//     const planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
-//     const planetObject = new THREE.Object3D();
-//     planetObject.position.x = orbitRadius;
-//     planetObject.add(planetMesh);
+function createSaturnRing(texturePath) {
 
-//     return {
-//         name,
-//         mesh: planetMesh,
-//         object: planetObject,
-//         orbitRadius,
-//         orbitSpeed,
-//         rotationSpeeds,
-//         angle: 0
-//     };
-// }
+    const innerRadius = 2.5;
+    const outerRadius = 3.5;
+    const thetaSegments = 60;
+
+    const ringGeometry = new THREE.RingGeometry(innerRadius, outerRadius, thetaSegments);
+    const ringTexture = textureLoader.load(texturePath);
+    const ringMaterial = new THREE.MeshBasicMaterial({
+        map: ringTexture,
+        side: THREE.DoubleSide
+    });
+
+    const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
+
+    // Rotate the ring
+    ringMesh.rotation.x = -Math.PI / 2;
+
+    return ringMesh;
+}
+
+
 
 function createPlanet({ name, texture, size, orbitRadius, orbitSpeed, rotationSpeeds }) {
     const planetGeometry = new THREE.SphereGeometry(size, 32, 32);
@@ -121,7 +140,7 @@ function createPlanet({ name, texture, size, orbitRadius, orbitSpeed, rotationSp
         object: planetObject,
         orbitRadius,
         orbitSpeed,
-        rotationSpeeds, // Ensure this is included
+        rotationSpeeds,
         angle: 0
     };
 }
@@ -130,13 +149,20 @@ function createPlanet({ name, texture, size, orbitRadius, orbitSpeed, rotationSp
 function updatePlanet(planet) {
     planet.angle += (1 / planet.orbitSpeed) * 0.001;
     planet.object.position.x = planet.orbitRadius * Math.cos(planet.angle);
-    planet.object.position.z = planet.orbitRadius * Math.sin(planet.angle);
+    planet.object.position.z = planet.orbitRadius  * Math.sin(planet.angle);
 }
 
 function rotatePlanet(planet, rotationSpeeds) {
     planet.rotation.x += rotationSpeeds.x;
     planet.rotation.y += rotationSpeeds.y;
     planet.rotation.z += rotationSpeeds.z;
+}
+
+function rotateSun() {
+    sun.rotation.x += 0.002;
+    sun.rotation.y += 0.002;
+    sun.rotation.z += 0.0001;
+
 }
 
 function createStars(amount) {
@@ -155,23 +181,25 @@ function moveCamera() {
     const move = document.body.getBoundingClientRect().top;
 
     // Calculate the distance from the sun
-    const distance = 2 * sun.geometry.parameters.radius;
+    const distance = 1.1 * sun.geometry.parameters.radius;
 
     // Calculate the angle based on the scroll position
     const angle = move * 0.0015;
 
     // Calculate the new position of the camera in a spiral pattern
     const x = distance * Math.cos(-angle);
-    const y = move * -0.004;
+    const y = move * -0.008;
     const z = distance * Math.sin(angle);
 
     // Set the position of the camera
     camera.position.set(x, y, z);
+
+   
 }
 
 document.body.onscroll = moveCamera;
 
 
 
-// Note: This refactoring assumes that the planets are defined in the planetsInfo array.
-// You would need to add the rest of the planets with their respective properties to the array.
+
+
